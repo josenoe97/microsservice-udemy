cc76df152a376fcfda08d1bfb91865d67cca60a6
José Edson Junior <edsonjunior9701@outlook.com>
2026-06-11 12:08:54 -0300

    feat: configuração do banco de dado

---
+++ GeekShopping/GeekShopping.ProductAPI/Model/Base/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.ProductAPI.Model.Base
+{
+    public class BaseEntity
+    {
+        
+        [Key]
+        [Column("id")]
+        public long Id { get; set; }
+    }
+}
