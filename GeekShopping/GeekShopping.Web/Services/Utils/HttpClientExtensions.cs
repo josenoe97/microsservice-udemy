23b9cf53d0537eb155865df52e92b2cfa72027e0
José Edson Junior <edsonjunior9701@outlook.com>
2026-06-17 14:37:11 -0300

    feat: implementação do metodos PutAsJson e PostAsJson

--- GeekShopping/GeekShopping.Web/Services/Utils/HttpClientExtensions.cs
+++ GeekShopping/GeekShopping.Web/Services/Utils/HttpClientExtensions.cs
@@ -1,15 +1,40 @@
-﻿namespace GeekShopping.Web.Services.Utils
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace GeekShopping.Web.Services.Utils
 {
     public static class HttpClientExtensions
     {
+        private static MediaTypeHeaderValue contentType = new MediaTypeHeaderValue("application/jason");
+
         public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
         {
             if(!response.IsSuccessStatusCode) throw new ApplicationException($"Something went wrong calling the API: " +
                                                                              $"{response.ReasonPhrase}");
             var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
 
-            return System.Text.Json.JsonSerializer.Deserialize<T>(dataAsString, 
-                new System.Text.Json.JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            return JsonSerializer.Deserialize<T>(dataAsString, 
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+
+        public static Task<HttpResponseMessage> PostAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content .Headers.ContentType = contentType;
+
+            return httpClient.PostAsync(url, content);
+        }
+
+        public static Task<HttpResponseMessage> PutAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content .Headers.ContentType = contentType;
+
+            return httpClient.PutAsync(url, content);
         }
     }
+
+    
 }
