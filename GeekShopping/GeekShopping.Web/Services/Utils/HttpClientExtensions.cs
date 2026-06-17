48504821b2b55e787eb5e124bed6117c879d30da
José Edson Junior <edsonjunior9701@outlook.com>
2026-06-17 14:07:03 -0300

    feat: criação do httpClientExtension para conectar front/back

---
+++ GeekShopping/GeekShopping.Web/Services/Utils/HttpClientExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace GeekShopping.Web.Services.Utils
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if(!response.IsSuccessStatusCode) throw new ApplicationException($"Something went wrong calling the API: " +
+                                                                             $"{response.ReasonPhrase}");
+            var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            return System.Text.Json.JsonSerializer.Deserialize<T>(dataAsString, 
+                new System.Text.Json.JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
