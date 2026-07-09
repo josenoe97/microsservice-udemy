0912946990238e330c089b3b0fbed3c945d0bd48
José Edson Junior <edsonjunior9701@outlook.com>
2026-07-09 11:12:55 -0300

    feat: modulo do identity completo

--- GeekShopping/foo/Pages/Account/Logout/LogoutOptions.cs
+++
@@ -1,7 +0,0 @@
-namespace foo.Pages.Logout;
-
-public static class LogoutOptions
-{
-    public static readonly bool ShowLogoutPrompt = true;
-    public static readonly bool AutomaticRedirectAfterSignOut = false;
-}
